15e81861cc3b52c26e6f273d40f2f5a3e31c3a1b
AubreyBellis <contactaubreybellis@gmail.com>
2017-07-14 16:22:31 -0400

    update

--- game.js
+++ game.js
@@ -1,31 +1,6 @@
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-'11'.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
 
 
-// $(document).ready(function () {
+// // $(document).ready(function () {
 //   //your code here
 
 // function whenClicked(){
@@ -37,35 +12,60 @@ window.onclick = function(event) {
 // })
 
 
-//Questions.. Variables
-// var quest1 =  
-// var quest2 =
-// var quest3 =
-// var quest4 = 
-// var quest5 = 
-// var quest6 = 
-// var quest7 =
-// var quest8 = 
-// var quest9 =
-// var quest10 =
-// var quest11 =
-// var quest12 =
-// var quest13 =
-// var quest14 =
-// var quest15 =
-// var quest16 = 
-// var quest17 =
-// var quest18 =
-// var quest19 =
-// var quest20 = 
-// var quest21 =
-// var quest22 =
-// var quest23 =
-// var quest24 =
-// var quest25 =
+///questions:
+var quest1 = 
+var quest2 =
+var quest3 =
+var quest4 =
+var quest5 = 
+var quest6 = 
+var quest7 =
+var quest8 = 
+var quest9 =
+var quest10 =
+var quest11 =
+var quest12 =
+var quest13 =
+var quest14 =
+var quest15 =
+var quest16 = 
+var quest17 =
+var quest18 =
+var quest19 =
+var quest20 = 
+var quest21 =
+var quest22 =
+var quest23 =
+var quest24 =
+var quest25 =
+
+////Answers: 
+var ans1 =
+var ans2 =
+var ans3 =
+var ans4 =
+var ans5 =
+var ans6 =
+var ans7 =
+var ans8 =
+var ans9 =
+var ans10 =
+var ans11 = 
+var ans12 =
+var ans13 = 
+var ans14 =
+var ans15 =
+var ans16 = 
+var ans17 = 
+var ans18 =
+var ans19 =
+var ans20 =
+var ans21 =
+var ans22 =
+var ans23 =
+var ans24 =
+var ans25 = 
 
 
 
 
-//Answers
-// });
