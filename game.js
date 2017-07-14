47272a7b50075c7ba1511d92100c63e23a7b28c6
AubreyBellis <contactaubreybellis@gmail.com>
2017-07-14 14:48:06 -0400

    modal

---
+++ game.js
@@ -0,0 +1,71 @@
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+'11'.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+// $(document).ready(function () {
+//   //your code here
+
+// function whenClicked(){
+//     alert('you did it.');
+// }
+// // When the user clicks the button, open the modal 
+// $('#11').on('click', function(){
+//    whenClicked();
+// })
+
+
+//Questions.. Variables
+// var quest1 =  
+// var quest2 =
+// var quest3 =
+// var quest4 = 
+// var quest5 = 
+// var quest6 = 
+// var quest7 =
+// var quest8 = 
+// var quest9 =
+// var quest10 =
+// var quest11 =
+// var quest12 =
+// var quest13 =
+// var quest14 =
+// var quest15 =
+// var quest16 = 
+// var quest17 =
+// var quest18 =
+// var quest19 =
+// var quest20 = 
+// var quest21 =
+// var quest22 =
+// var quest23 =
+// var quest24 =
+// var quest25 =
+
+
+
+
+//Answers
+// });
