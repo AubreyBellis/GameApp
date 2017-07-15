c9554e20f0726059f27c0df3e5078a22482ebde3
AubreyBellis <contactaubreybellis@gmail.com>
2017-07-15 16:37:22 -0400

    functioning

--- game.js
+++ game.js
@@ -92,7 +92,128 @@ $("#52").on('click',function(){
     }
 });
 //Third row of columns of buttons from top to bottom
-
+$("#13").on('click',function(){
+    var x = prompt(quest11)
+    if (x === ans11) {
+        alert('You got it!')
+    } else {
+        alert('Sorry, that is wrong!');
+    }
+});
+$("#23").on('click',function(){
+    var x = prompt(quest12)
+    if (x === ans12) {
+        alert('You got it!')
+    } else {
+        alert('Sorry, that is wrong!');
+    }
+});
+$("#33").on('click',function(){
+    var x = prompt(quest13)
+    if (x === ans13) {
+        alert('You got it!')
+    } else {
+        alert('Sorry, that is wrong!');
+    }
+});
+$("#43").on('click',function(){
+    var x = prompt(quest14)
+    if (x === ans14) {
+        alert('You got it!')
+    } else {
+        alert('Sorry, that is wrong!');
+    }
+});
+$("#53").on('click',function(){
+    var x = prompt(quest15)
+    if (x === ans15) {
+        alert('You got it!')
+    } else {
+        alert('Sorry, that is wrong!');
+    }
+});
+//fourth column of buttons from top to bottom 
+$("#14").on('click',function(){
+    var x = prompt(quest16)
+    if (x === ans16) {
+        alert('You got it!')
+    } else {
+        alert('Sorry, that is wrong!');
+    }
+});
+$("#24").on('click',function(){
+    var x = prompt(quest17)
+    if (x === ans17) {
+        alert('You got it!')
+    } else {
+        alert('Sorry, that is wrong!');
+    }
+});
+$("#34").on('click',function(){
+    var x = prompt(quest18)
+    if (x === ans18) {
+        alert('You got it!')
+    } else {
+        alert('Sorry, that is wrong!');
+    }
+});
+$("#44").on('click',function(){
+    var x = prompt(quest19)
+    if (x === ans19) {
+        alert('You got it!')
+    } else {
+        alert('Sorry, that is wrong!');
+    }
+});
+$("#54").on('click',function(){
+    var x = prompt(quest20)
+    if (x === ans20) {
+        alert('You got it!')
+    } else {
+        alert('Sorry, that is wrong!');
+    }
+});
+//Fifth row of columns from top to bottom 
+$("#15").on('click',function(){
+    var x = prompt(quest21)
+    if (x === ans21) {
+        alert('You got it!')
+    } else {
+        alert('Sorry, that is wrong!');
+    }
+});
+$("#25").on('click',function(){
+    var x = prompt(quest22)
+    if (x === ans22) {
+        alert('You got it!')
+    } else {
+        alert('Sorry, that is wrong!');
+    }
+});
+$("#35").on('click',function(){
+    var x = prompt(quest23)
+    if (x === ans23) {
+        alert('You got it!')
+    } else {
+        alert('Sorry, that is wrong!');
+    }
+});
+$("#45").on('click',function(){
+    var x = prompt(quest24)
+    if (x === ans24) {
+        alert('You got it!')
+    } else {
+        alert('Sorry, that is wrong!');
+    }
+});
+$("#55").on('click',function(){
+    var x = prompt(quest25)
+    if (x === ans25) {
+        alert('You got it!')
+    } else {
+        alert('Sorry, that is wrong!');
+    }
+});
 ///questions:
 var quest1 = "Who is the known as the 'Lady with the lamp?'";
 var quest2 = 'Who was the first christian roman emperor?';
@@ -101,24 +222,24 @@ var quest4 = 'Who became an American Chess Grandmaster at age 15?';
 var quest5 = "'Who is known as Americas Cowboy Philosopher?'";
 var quest6 = "'I have a dream!'";
 var quest7 = "'I can resist anything but temptation'";
-// var quest8 = "' Two roads diverged in the a wood and I took the one less traveled.'";
-// var quest9 = "' Believe you can and you're halfway there'";
-// var quest10 = "'I didn’t fail the test. I just found 100 ways to do it wrong.'";
-// var quest11 = 'What does AA stand for?';
-// var quest12 = 'What does CIA stand for?';
-// var quest13 = 'What does EPA stand for?';
-// var quest14 = 'What does AWOL stand for?';
-// var quest15 = 'What does CAD stand for?;
-// var quest16 = "What city is known as the 'City of Lakes'";
-// var quest17 = 'What is the oldest State Capital in the US?';
-// var quest18 = "What city is known as the 'Big Peach?'";
-// var quest19 = "What city is known as the 'City of Roses?'";
-// var quest20 = 'What was the first capital in California?'
-// var quest21 = 'What is a baby whale called?'
-// var quest22 = 'A group of Doves is called what?'
-// var quest23 = 'A snake hears with what organ?'
-// var quest24 = 'From what country does the Dalmation dog breed come from?;
-// var quest25 = 'In British Columbia it's illegal to kill what animal?'
+var quest8 = "' Two roads diverged in the a wood and I took the one less traveled.'";
+var quest9 = "' Believe you can and you're halfway there'";
+var quest10 = "'I didn’t fail the test. I just found 100 ways to do it wrong.'";
+var quest11 = 'What does AA stand for?';
+var quest12 = 'What does CIA stand for?';
+var quest13 = 'What does EPA stand for?';
+var quest14 = 'What does AWOL stand for?';
+var quest15 = 'What does CAD stand for?';
+var quest16 = "What city is known as the 'City of Lakes'";
+var quest17 = 'What is the oldest State Capital in the US?';
+var quest18 = "What city is known as the 'Big Peach?'";
+var quest19 = "What city is known as the 'City of Roses?'";
+var quest20 = 'What was the first capital in California?';
+var quest21 = 'What is a baby whale called?';
+var quest22 = 'A group of Doves is called what?';
+var quest23 = 'A snake hears with what organ?';
+var quest24 = 'From what country does the Dalmation dog breed come from?';
+var quest25 = 'In British Columbia its illegal to kill what animal?';
 
 ////Answers: 
 var ans1 = 'Who is Florence Nightingale';
@@ -128,48 +249,54 @@ var ans4 = 'Who is Bobby Fischer';
 var ans5 = 'Who is Will Rogers';
 var ans6 = 'Who is Martin Luther King';
 var ans7= 'Who is Oscar Wilde';
-// var ans8 ='Who is Robert Frost';
-// var ans9 = 'Who is Theodore Roosevelt';
-// var ans10 = 'Who is Benjamin Franklin';
-// var ans11 = 'What is Alcoholics Anonymous';
-// var ans12 = 'What is the Central Intelligence Agency';
-// var ans13 = 'What is the Environmental Protection Agency';
-// var ans14 = 'What is Abscence without leave';
-// var ans15 = 'What is Computer Aided Design';
-// var ans16 = 'What is Minneapolis, Minnesota';
-// var ans17 = 'What is Santa Fe, New Mexico';
-// var ans18 = 'What is Atlanta, Georgia';
-// var ans19 = 'What is Portland, Oregon';
-// var ans20 = 'What is San Jose';
-// var ans21 = 'What is a calf';
-// var ans22 = 'What is a dule';
-// var ans23 = 'What is a tongue';
-// var ans24 = 'What is Yugoslavia'
-// var ans25 = 'What is Sasquatch'
+var ans8 ='Who is Robert Frost';
+var ans9 = 'Who is Theodore Roosevelt';
+var ans10 = 'Who is Benjamin Franklin';
+var ans11 = 'What is Alcoholics Anonymous';
+var ans12 = 'What is the Central Intelligence Agency';
+var ans13 = 'What is the Environmental Protection Agency';
+var ans14 = 'What is Abscence without leave';
+var ans15 = 'What is Computer Aided Design';
+var ans16 = 'What is Minneapolis, Minnesota';
+var ans17 = 'What is Santa Fe, New Mexico';
+var ans18 = 'What is Atlanta, Georgia';
+var ans19 = 'What is Portland, Oregon';
+var ans20 = 'What is San Jose';
+var ans21 = 'What is a calf';
+var ans22 = 'What is a dule';
+var ans23 = 'What is a tongue';
+var ans24 = 'What is Yugoslavia';
+var ans25 = 'What is Sasquatch';
 
-$(document).ready(function(){
-  $('input[type=text][title],input[type=password][title],textarea[title]').each(function(i){
-    $(this).addClass('input-prompt-' + i);
-    var promptSpan = $('<span class="input-prompt"/>');
-    $(promptSpan).attr('id', 'input-prompt-' + i);
-    $(promptSpan).append($(this).attr('title'));
-    $(promptSpan).click(function(){
-      $(this).hide();
-      $('.' + $(this).attr('id')).focus();
-    });
-    if($(this).val() != ''){
-      $(promptSpan).hide();
-    }
-    $(this).before(promptSpan);
-    $(this).focus(function(){
-      $('#input-prompt-' + i).hide();
-    });
-    $(this).blur(function(){
-      if($(this).val() == ''){
-        $('#input-prompt-' + i).show();
-      }
-    });
-  });
-});
+
+
+
+
+///trying to customize the alert
+
+// $(document).ready(function(){
+//   $('input[type=text][title],input[type=password][title],textarea[title]').each(function(i){
+//     $(this).addClass('input-prompt-' + i);
+//     var promptSpan = $('<span class="input-prompt"/>');
+//     $(promptSpan).attr('id', 'input-prompt-' + i);
+//     $(promptSpan).append($(this).attr('title'));
+//     $(promptSpan).click(function(){
+//       $(this).hide();
+//       $('.' + $(this).attr('id')).focus();
+//     });
+//     if($(this).val() != ''){
+//       $(promptSpan).hide();
+//     }
+//     $(this).before(promptSpan);
+//     $(this).focus(function(){
+//       $('#input-prompt-' + i).hide();
+//     });
+//     $(this).blur(function(){
+//       if($(this).val() == ''){
+//         $('#input-prompt-' + i).show();
+//       }
+//     });
+//   });
+// });
 
 
